37677f62f8f9379071469ed28e598275e8247c9a
Alexander Schimanko <as323@hdm-stuttgart.de>
2024-01-18 21:23:23 +0100

    implemented submitTableAvatar in table-selection-tab-controller.js

--- skat_server/src/WEB-INF/js/table-selection-tab-controller.js
+++ skat_server/src/WEB-INF/js/table-selection-tab-controller.js
@@ -72,11 +72,13 @@ export default class TableSelectionTabController extends TabController {
 				console.log("tableAvatarElement :")
 				console.log(tableAvatarElement)
 				tableAvatarElement.src = `/services/documents/${table.avatar.identity}?cache-bust=${Date.now()}`;
+				tableAvatarElement.addEventListener("drop", event => this.submitTableAvatar(table, tableAvatarElement, event.dataTransfer.files[0]));
 		
 				// Seats avatar
 				const seatClasses = ["fore", "middle", "rear"];
 				seatClasses.forEach((seatClass, i) => {
 					const playerAvatarElement = tableSelectionRow.querySelector(`.${seatClass}`);
+					playerAvatarElement.addEventListener("click", event => this.occupyTablePosition(table, i))
 					console.log("playerAvatarElement :" + i)
 					console.log(playerAvatarElement)
 
@@ -125,20 +127,23 @@ export default class TableSelectionTabController extends TabController {
 	 */
 	async submitTableAvatar (table, avatarElement, avatarFile) {
 		if (this.properties.sessionOwner.group !== "ADMIN") return;
-
+		console.log(this.properties.sessionOwner.group);
 		this.messageElement.value = "";
 		try {
 			let response;
 
 			response = await fetch("/services/documents", { method: "POST", headers: {"Content-Type": avatarFile.type}, body: avatarFile });
 			if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
+
 			const avatarReference = parseInt(await response.text());
-			if (this.properties.sessionOwner.avatar.identity === avatarReference) return;
-			this.properties.sessionOwner.avatar.identity = avatarReference;
+			console.log(table.avatar.identity);
+
+			if (table.avatar.identity === avatarReference) return;
+			table.avatar.identity = avatarReference;
 			
-			response = await fetch("/services/tables", { method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(this.properties.sessionOwner) });
+			response = await fetch("/services/tables", { method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(table) });
 			if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
-			this.properties.sessionOwner.version += 1;
+			table.version += 1;
 
 			this.rootSection.querySelector("img.table").src = "/services/documents/" + avatarReference + "?cache-bust=" + Date.now();
 
@@ -156,13 +161,14 @@ export default class TableSelectionTabController extends TabController {
 	 * @param {Object} table the table
 	 * @param {number} tablePosition the table position
 	 */
-	occupyTablePosition (table, tablePosition) {
+	async occupyTablePosition (table, tablePosition) {
 		if (this.properties.sessionOwner.group === "ADMIN") return;
 
 		this.messageElement.value = "";
 		try {
-			// TODO
-
+			response = await fetch("/services/tables/"+ table.identity +"/players/" + tablePosition, { method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(this.properties.sessionOwner) });
+			if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
+			
 			const playButton = this.tabControls.find(button => button.classList.contains("table-play"));
 			playButton.click();
 		} catch (error) {
